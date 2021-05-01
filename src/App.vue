e77a5e1638d0f670a69b4cb663477a77305726af
surmount96 <ayanwoye74@gmail.com>
2021-05-01 18:18:23 +0100

    conversion of card screen

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,136 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="lg:px-20 height">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+$color-gray : #D3DCE6;
+$color-black: #131313;
+$eye-color: #575A65;
+$border-radius:20px;
+
+@font-face {
+  src: url("./assets/Graphik-Font-Family/GraphikRegular.otf");
+  font-family: Graphik;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  font-family: "Graphik";
+  font-size: 16px;
+  color: $color-gray;
+  overflow: hidden;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.height {
+  height: 100vh;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.bg-dark{
+  background-color: $color-black;
+}
+.text-gray{
+  color:$color-gray;
+}
+
+.rounded-custom{
+  border-radius: $border-radius;
+}
+
+.col{
+  padding: 0;
+  width:499px;
+}
+
+.h-50{
+  height:305px;
+}
+.bg-image{
+  width: 364px;
+  background-image: url('./assets/patricia-icon.png');
+  background-repeat: no-repeat;
+  background-position: left;
+  background-size: cover;
+  height: 100%;
+}
+.mx-4{
+  margin:0 20px ;
+}
+
+.px-40-custom{
+  padding: 0 70px;
+}
+
+.px-12-custom{
+  padding: 25px 33px;
+}
+.h4{
+  font-size: 32px;
+}
+.h5{
+  font-size: 23px;
+}
+
+.card-text{
+  font-size: 14px;
+}
+
+@media screen and (max-width: 600px){
+  .col{
+    padding: 0;
+    width:399px;
+    margin: 0 auto;
+  }
+
+  .h-50{
+    height:245px;
+  }
+
+  .height {
+    height: auto;
+  }
+  .px-2-custom{
+    padding: 0 3px;
+  }
+
+  .rounded-custom{
+    border-radius: 14px;
+  }
+
+}
+
+@media screen and (max-width: 450px){
+  .col{
+    padding: 0;
+    width:340px;
+    margin: 0 auto;
+  }
+
+  .bg-image{
+    width: 90%;
+  }
+
+  .bg-dark{
+    overflow: hidden;
+  }
+  .h-50{
+    height:35vh;
+  }
+
+  .h5,.h4{
+    font-size: 16px;
+  }
+
+  #app {
+    font-size: 12px;
+  }
+
+  .height {
+    height: auto;
+  }
+
+  .rounded-custom{
+    border-radius: 13px;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,136 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <div class="flex lg:items-center flex-wrap lg:justify-start justify-center h-25 w-full">
+      <div class="lg:flex lg:items-center lg:flex-wrap">
+        <div class="lg:flex lg:items-baseline relative hidden lg:w-7/12 w-full lg:mb-0 md:mb-0 mb-6">
+          <div class="flex items-baseline">
+            <div class="pb-10 pr-6">
+              <img src="@/assets/patricia-logo.svg" alt="patricia-logo">
+            </div>
+            <div class="flex flex-wrap items-baseline">
+              <div class="w-8/12">
+                <img src="@/assets/patricia.svg" alt="patricia"  class="h-50 object-cover ">
+              </div>
+              <div class="flex w-4/12">
+                <img src="@/assets/VALID-THRU.svg" alt="valid-thru">
+                <p class="ml-3 mr-5">
+                  10/12
+                </p>
+              </div>
+            </div>
+          </div>
+<!--          <div class="flex items-center w-3/12">-->
+<!--            <div class="flex">-->
+<!--              <img src="@/assets/VALID-THRU.svg" alt="valid-thru">-->
+<!--              <p class="ml-3">-->
+<!--                10/12-->
+<!--              </p>-->
+<!--              <p>-->
+<!--                422-->
+<!--              </p>-->
+<!--            </div>-->
+<!--          </div>-->
+          <div class="absolute top-0 right-0 mr-12">
+            <div class="flex justify-between flex-col h-50 pt-8">
+              <img src="@/assets/Mask-open.svg" alt="patricia-logo" class="cursor-pointer">
+              <p>
+                422
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-5/12 w-full lg:mb-0 md:mb-0 mb-6 lg:pt-0 md:pt-0 pt-5">
+          <div class="bg-dark rounded-custom col h-50 relative lg:ml-auto mx-auto">
+            <div class="bg-image"></div>
+            <div class="absolute flex flex-col justify-between top-0 w-full px-12-custom h-full">
+              <div class="flex justify-between">
+                <img src="@/assets/patricia-logo.svg" alt="patricia-logo">
+                <img src="@/assets/Mask.svg" alt="patricia-logo" class="cursor-pointer" @click="show = !show">
+              </div>
+              <div class="flex justify-between items-center w-full text-gray">
+                <p class="h5">
+                    <span v-show="show">
+                      9784
+                    </span>
+                  <span class="h4" v-show="!show">
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                    </span>
+                </p>
+                <p class=" h5">
+                    <span v-show="show">
+                      9784
+                    </span>
+                  <span class="h4"  v-show="!show">
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                    </span>
+                </p>
+                <p class=" h5">
+                    <span v-show="show">
+                      9784
+                    </span>
+                  <span class="h4"  v-show="!show">
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                    </span>
+                </p>
+                <p class=" h5">
+                  9784
+                </p>
+              </div>
+              <div class="flex justify-between items-center">
+                <p class="card-text">
+                  Cardholder name
+                </p>
+                <div class="flex items-center">
+                  <div class="flex mx-4">
+                    <img src="@/assets/VALID-THRU.svg" alt="valid-thru">
+                    <p class="px-2">
+                      10/12
+                    </p>
+                  </div>
+                  <div class="flex items-center">
+                    <p class="mr-2">
+                      cvv
+                    </p>
+                    <p class="h5">
+                      &#8226;
+                      &#8226;
+                      &#8226;
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="lg:px-24">
+      <div class="bg-dark rounded-custom col h-50 flex items-center px-12 lg:mb-0 md:mb-0 mb-6">
+        <div class="flex justify-between items-center text-gray w-full">
+          <p class="h5">
+            9784
+          </p>
+          <p class="h5">
+            9784
+          </p>
+          <p class="h5">
+            9784
+          </p>
+          <p class="h5">
+            9784
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,8 +140,25 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      show:false
+    };
+  },
   components: {
     HelloWorld
   }
 }
 </script>
+
+<style>
+.h-25{
+  height: 70vh;
+}
+
+@media screen and (max-width: 600px) {
+  .h-25{
+    height: auto;
+  }
+}
+</style>
